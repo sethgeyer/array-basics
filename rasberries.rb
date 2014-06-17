8f86e98667d58fd0d2c44e683bd9fc3388cd3511
Seth Geyer <seth.geyer@gmail.com>
2014-06-17 16:54:07 -0600

    Updated rasberries to not impact the original array

--- rasberries.rb
+++ rasberries.rb
@@ -6,7 +6,8 @@ fruits = %w(apples oranges grapes bananas)
 
 apple_location = fruits.index("apples")
 
-fruits.insert(apple_location + 1, "raspberries")
+more_fruits = fruits.dup.insert(apple_location + 1, "raspberries")
 
 
-print fruits
+print more_fruits
+puts
